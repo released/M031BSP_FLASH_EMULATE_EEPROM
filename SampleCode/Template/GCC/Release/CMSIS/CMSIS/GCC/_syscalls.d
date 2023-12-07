3f8a27570ce6a4c875fec27a114c2cf976abd46d
Released <released.chen@gmail.com>
2023-12-07 17:52:08 +0800

    update
    update

--- SampleCode/Template/GCC/Release/CMSIS/CMSIS/GCC/_syscalls.d
+++
@@ -1,2 +0,0 @@
-CMSIS/CMSIS/GCC/_syscalls.o: \
- D:/SourceCode/_Avery_M031/M031BSP_FLASH_EMULATE_EEPROM/Library/Device/Nuvoton/M031/Source/GCC/_syscalls.c
--- SampleCode/Template/GCC/Release/CMSIS/CMSIS/GCC/startup_M031Series.d
+++
@@ -1,2 +0,0 @@
-CMSIS/CMSIS/GCC/startup_M031Series.o: \
- D:/SourceCode/_Avery_M031/M031BSP_FLASH_EMULATE_EEPROM/Library/Device/Nuvoton/M031/Source/GCC/startup_M031Series.S
